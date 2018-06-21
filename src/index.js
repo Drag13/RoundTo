e17bc7cd3c5d5668af479b5dd290d8754cb02e82
drag13 <qwark@list.ru>
2018-06-21 21:38:24 +0300

    Added eslint and prettier support

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@
  * Rounds the number with selected precision
  * @param {number} num - number to round.
  * @param {number} precision - precision to use
- * @returns {number} - rounded number.
+ * @return {number} - rounded number.
  */
 function roundTo(num, precision) {
     const lvl = Math.pow(10, precision);
-    return Math.round((num + Number.EPSILON) * lvl) / lvl
+    return Math.round((num + Number.EPSILON) * lvl) / lvl;
 }
 
 module.exports = roundTo;
--- tests/correctvalues.test.js
+++ tests/correctvalues.test.js
@@ -1,6 +1,6 @@
-const chai = require('chai'),
-    roundTo = require('../src/index'),
-    expect = chai.expect;
+const chai = require('chai');
+const roundTo = require('../src/index');
+const expect = chai.expect;
 
 const testData = [
     { number: 0, precision: 0, expected: 0 },
@@ -15,7 +15,7 @@ const testData = [
     { number: 0.55, precision: 1, expected: 0.6 },
     { number: 0.55, precision: 2, expected: 0.55 },
     { number: 0.555, precision: 2, expected: 0.56 },
-    { number: 0.50000, precision: 4, expected: 0.5 },
+    { number: 0.5, precision: 4, expected: 0.5 },
     { number: 0.5555, precision: 4, expected: 0.5555 },
 
     { number: 1.3555, precision: 2, expected: 1.36 },
@@ -26,13 +26,14 @@ const testData = [
     { number: 0.43499999999999994, precision: 3, expected: 0.435 },
     { number: 0.43499999999999994, precision: 4, expected: 0.435 },
 
-    { number: 1, precision: 0, expected: 1 }
+    { number: 1, precision: 0, expected: 1 },
 ];
 
 describe('roundTo function', () => {
-
-    testData.forEach((input) => {
-        it(`should round ${input.number} with precision ${input.precision} to ${input.expected}`, () => {
+    testData.forEach(input => {
+        it(`should round ${input.number} with precision ${input.precision} to ${
+            input.expected
+        }`, () => {
             const rounded = roundTo(input.number, input.precision);
             expect(rounded).to.be.equal(input.expected);
         });
